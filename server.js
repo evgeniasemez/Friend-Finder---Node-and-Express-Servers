e9a075c1fa90549d4f5d9366ebf0ce1c20c8aa97
Evgenia Semez <esemez@indeed.com>
2019-05-14 22:01:53 -0700

    adding some extra

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+// var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () {
 
     app.get("/", function (req, res) {
-        res.json(path.join(__dirname, "app/public/index.html"));
+        // res.json(path.join(__dirname, "public/index.html"));
+        console.log("app.get('/' worked")
+        res.sendFile(path.join(__dirname, "app/public/home.html"));
     });
 })
